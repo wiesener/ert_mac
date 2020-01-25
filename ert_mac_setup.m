dddd5400e0da0903e5ef9b9038d8329ed6faa65e
constantin <cwiesener@sensorstim.com>
2020-01-25 14:14:08 +0100

    Changed for new Matlab version
    *  tested under 2019b macOSX

--- ert_mac_setup.m
+++
@@ -1,6 +0,0 @@
-function ert_mac_setup()
-curpath = pwd;
-tgtpath = curpath(1:end-length('/ert_mac'));
-addpath(fullfile(tgtpath, 'ert_mac'));
-savepath;
-disp('ERT_MAC Target Path Setup Complete.');
--- sl_customization.m
+++
@@ -1,3 +0,0 @@
-function sl_customization(cm)
-    cm.ExtModeTransports.add('ert_mac.tlc', 'tcpip', 'ext_comm', 'Level1');
-end
